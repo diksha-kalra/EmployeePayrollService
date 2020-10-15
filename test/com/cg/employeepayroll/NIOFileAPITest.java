8801b032b2df21d9c4977adcc4ccbb51536e1213
diksha-kalra <kalradiksha11@gmail.com>
2020-10-15 20:44:14 +0530

    Added: watch service to watch particular directory along with all files and sub directories

--- test/com/cg/employeepayroll/NIOFileAPITest.java
+++ test/com/cg/employeepayroll/NIOFileAPITest.java
@@ -5,7 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.IntStream;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -40,10 +39,16 @@ public class NIOFileAPITest {
 			}
 			Assert.assertTrue(Files.exists(tempFile));
 		});
-
 		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
 		Files.newDirectoryStream(playPath).forEach(System.out::println);
 		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
 				.forEach(System.out::println);
+	}
+
+	@Test
+	public void givenADirectoryWhenWatchedListsAllTheActivities() throws IOException, InterruptedException {
+		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new java8WatchServiceExample(dir).processEvents();
 	}
 }
