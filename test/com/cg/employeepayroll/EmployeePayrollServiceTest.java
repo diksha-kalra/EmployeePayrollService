7d6dd26e07068ee48243aa9dc0b671f38e334d2d
diksha-kalra <kalradiksha11@gmail.com>
2020-10-15 21:46:33 +0530

    Added: storing employee payroll data in file

--- test/com/cg/employeepayroll/EmployeePayrollServiceTest.java
+++ test/com/cg/employeepayroll/EmployeePayrollServiceTest.java
@@ -1,7 +1,6 @@
 package com.cg.employeepayroll;
 
 import java.util.Arrays;
-import org.junit.Assert;
 import org.junit.Test;
 import com.cg.employeepayroll.EmployeePayrollService.IOService;
 
@@ -17,8 +16,5 @@ public class EmployeePayrollServiceTest {
 		EmployeePayrollService employeePayrollService;
 		employeePayrollService=new EmployeePayrollService(Arrays.asList(arrayOfEmps));
 		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
-//		employeePayrollService.printData(IOService.FILE_IO);
-//		long entries=employeePayrollService.countEnteries(IOService.FILE_IO);
-//		Assert.assertEquals(3,entries);
 	}
 }
