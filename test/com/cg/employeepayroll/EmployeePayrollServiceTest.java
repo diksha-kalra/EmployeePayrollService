ef937ddfaa5589c11a02bff7c0d6f5074ae04219
diksha-kalra <kalradiksha11@gmail.com>
2020-10-15 23:18:11 +0530

    Refactor: storing employees with different id

--- test/com/cg/employeepayroll/EmployeePayrollServiceTest.java
+++ test/com/cg/employeepayroll/EmployeePayrollServiceTest.java
@@ -12,8 +12,8 @@ public class EmployeePayrollServiceTest {
 	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEnteries() {
 		EmployeePayrollData[] arrayOfEmps= {
 				new EmployeePayrollData(1,"Diksha kalra", 100000.0),
-				new EmployeePayrollData(1,"Bill Gates", 200000.0),
-				new EmployeePayrollData(1,"mark Zuckerberg", 300000.0)
+				new EmployeePayrollData(2,"Bill Gates", 200000.0),
+				new EmployeePayrollData(3,"mark Zuckerberg", 300000.0)
 		};
 		EmployeePayrollService employeePayrollService;
 		employeePayrollService=new EmployeePayrollService(Arrays.asList(arrayOfEmps));
